d333a57385e488387851c845d13f56555d5baa44
Yannick <yannickhj@gmail.com>
2018-01-18 19:00:41 +0100

    dashboard test/styling

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Foundation for Sites</title>
+    <title>Benno's Sportschool</title>
     <link rel="stylesheet" href="css/app.css">
   </head>
 
@@ -41,20 +41,38 @@
           </li>
         </ul>
       </div>
-      <div class="top-bar-right nav-login">
-        <?php
+      <div class="top-bar-right">
+        <ul class="menu">
+          <?php
           if (isset($_SESSION['lid_id'])) {
-            echo'<form action="includes/logout.inc.php" method="POST">
-                 <button type="submit" name="submit">Logout</button>
-                 </form>';
+            echo'<li><form action="includes/logout.inc.php" method="POST">
+                 <button class="button" type="submit" name="submit">Logout</button>
+                 </form></li>';
           } else {
-            echo'<form action="includes/login.inc.php" method="POST">
-                <input type="text" name="uid" placeholder="Gebruikersnaam/email">
-                <input type="password" name="pwd" placeholder="Wachtwoord">
-                <button type="submit" name="submit">Login</button>
-                <a href="signup.php">Aanmelden</a>
-                </form>';
+            echo'<li><form action="includes/login.inc.php" method="POST">
+                <input type="text" name="uid" placeholder="Gebruikersnaam/email" required></li>
+                <li><input type="password" name="pwd" placeholder="Wachtwoord" required></li>
+                <li><button class="button" type="submit" name="submit">Login</button>
+                </form></li>';
           }
         ?>
+        </ul>
       </div>
     </div>
+    <?php
+          if (isset($_SESSION['lid_id'])) {
+    $sql = "SELECT * FROM leden WHERE lid_uid = '" . $_SESSION['lid_uid'] . "'";
+    $result = mysqli_query($conn, $sql);
+    
+    $row = mysqli_fetch_array($result);
+    echo "Hello, " . $row['lid_voornaam'] . " (" . $row['lid_email'] . ").";
+          } else {
+            echo'
+    <div class="hero-img">
+      <div class="hero-content">
+        <h1>Bennos Sportschool</h1>
+        <h4>Lorem ipsum dolor sit amet.<a href="signup.php">Meld je aan</a></h4>
+      </div>
+    </div>'; 
+          }
+    ?>
