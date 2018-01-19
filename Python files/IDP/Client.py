1213af4f4fe6649b1077e4ec8e8ed1368f7eef98
B9a9s9 <bas.vaneck@ziggo.nl>
2018-01-19 16:07:59 +0100

    Updated

--- Python files/IDP/Client.py
+++ Python files/IDP/Client.py
@@ -5,10 +5,18 @@ host = ''
 port = 5555
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server_address = ('192.168.42.1', 5555)
+server_address = ('192.168.42.1', port)
 s.connect(server_address)
 
 data = input('wat wil je sturen')
+s.send(str.encode(data))
+reply = s.recv(2048)
+print(reply.decode('utf-8'))
+s.close()
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(server_address)
+data = 'nee'
 s.sendall(str.encode(data))
 reply = s.recv(2048)
 print(reply.decode('utf-8'))
