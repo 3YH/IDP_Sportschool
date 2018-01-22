0f51c048e747670cfc8121504275ed20f5cf3a05
Yannick <yannickhj@gmail.com>
2018-01-22 21:33:39 +0100

    frontend homepage

--- footer.php
+++ footer.php
@@ -2,5 +2,39 @@
   <script src="bower_components/what-input/dist/what-input.js"></script>
   <script src="bower_components/foundation-sites/dist/js/foundation.js"></script>
   <script src="js/app.js"></script>
+	<script src="js/parallax.min.js"></script>
+  <script>
+		$(document).ready(function () {
+			// Add smooth scrolling to all links
+			$("a").on('click', function (event) {
+
+				// Make sure this.hash has a value before overriding default behavior
+				if (this.hash !== "") {
+					// Prevent default anchor click behavior
+					event.preventDefault();
+
+					// Store hash
+					var hash = this.hash;
+
+					// Using jQuery's animate() method to add smooth page scroll
+					// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+					$('html, body').animate({
+						scrollTop: $(hash).offset().top
+					}, 800, function () {
+
+						// Add hash (#) to URL when done scrolling (default click behavior)
+						window.location.hash = hash;
+					});
+				} // End if
+			});
+		});
+  </script>
+  <script>
+    $('.top-bar').on('sticky.zf.stuckto:top', function(){
+  $(this).addClass('sticky-padding');
+}).on('sticky.zf.unstuckfrom:top', function(){
+  $(this).removeClass('sticky-padding');
+})
+    </script>
 </body>
 </html>
