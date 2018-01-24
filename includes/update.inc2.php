0bd7b71f732a7a03bd8916d861437eed6347bde6
Yannick <yannickhj@gmail.com>
2018-01-24 18:20:25 +0100

    update profile update

--- includes/update.inc2.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    include 'dbh.inc.php';
-
-            $sql = "UPDATE leden SET lid_voornaam='$_POST[lid_voornaam]' WHERE lid_uid = '$uid'";
-            mysqli_query($conn, $sql);
-
-            header("Location: ../update.php?update=success");
-            exit();
