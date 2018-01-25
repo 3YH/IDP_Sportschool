536bb60db69b23f8027a638e00e6ed9cbfbd8846
B9a9s9 <bas.vaneck@ziggo.nl>
2018-01-25 16:14:33 +0100

    finished

--- Python files/IDP/sql_codes.py
+++ Python files/IDP/sql_codes.py
@@ -1,8 +1,7 @@
 met = 'SELECT met_waarde FROM apparaat WHERE apparaat_naam ='
 gewicht = 'SELECT lid_gewicht FROM leden WHERE lid_id ='
-code = 'SELECT lid_id FROM leden WHERE lid_id ='
+code = 'SELECT isLoggedIn FROM leden WHERE lid_id ='
 resultaat = "INSERT INTO resultaat(lid_id, apparaat_id, tijd, cal, gem_snelheid, afstand) VALUES ("
 apparaat_code= 'SELECT apparaat_id FROM apparaat WHERE UPPER(apparaat_naam) ='
-
-
+status= 'UPDATE leden SET isLoggedIn = '
 
