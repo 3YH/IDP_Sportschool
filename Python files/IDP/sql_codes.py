fedd5d33532a258b15f532182193ddd68c2d5ca9
B9a9s9 <bas.vaneck@ziggo.nl>
2018-01-25 15:24:03 +0100

    bugfixes

--- Python files/IDP/sql_codes.py
+++ Python files/IDP/sql_codes.py
@@ -1,5 +1,8 @@
 met = 'SELECT met_waarde FROM apparaat WHERE apparaat_naam ='
-gewicht = 'SELECT met_waarde FROM apparaat WHERE apparaat_naam ='
+gewicht = 'SELECT lid_gewicht FROM leden WHERE lid_id ='
+code = 'SELECT lid_id FROM leden WHERE lid_id ='
+resultaat = "INSERT INTO resultaat(lid_id, apparaat_id, tijd, cal, gem_snelheid, afstand) VALUES ("
+apparaat_code= 'SELECT apparaat_id FROM apparaat WHERE UPPER(apparaat_naam) ='
 
 
 
