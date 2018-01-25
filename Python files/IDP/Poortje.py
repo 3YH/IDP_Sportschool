d0f7c2d16cd2e29286aa7cd2842cccb0d906166a
Dennis Frijlink <dennisfrijlink@gmail.com>
2018-01-25 13:45:22 +0100

    Done met input

--- Python files/IDP/Poortje.py
+++ Python files/IDP/Poortje.py
@@ -1,9 +1,10 @@
 import RPi.GPIO as GPIO
 import time
+import socket
 
 host = ''
 port = 5555
-server_address = ('192.168.42.1', 5555)
+server_address = ('145.89.252.97', port)
 
 
 led_groen = 5
@@ -14,24 +15,26 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(led_groen, GPIO.OUT)
 GPIO.setup(led_rood, GPIO.OUT)
 
-GPIO.output(led_groen, 1)
+GPIO.output(led_groen, 0)
+GPIO.output(led_rood, 0)
 
-def poortje():
+while True:
     code = input ("wat is je code: ")
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                        #Open een socket
     s.connect(server_address)
     s.sendall(str.encode("code:"+code))
-    reply = s.recv(2048)
+    answer = s.recv(2048)
+    reply = answer.decode('utf-8')
     s.close ()
 
     if reply == "none":
         GPIO.output(led_rood, 1)
-        sleep(1)
-        GPIO.ouput(led_groen, 0)
+        time.sleep(1)
+        GPIO.output(led_rood, 0)
     else:
         GPIO.output(led_groen, 1)
-        sleep(2)
-        GPIO.output(led_rood, 0)
+        time.sleep(2)
+        GPIO.output(led_groen, 0)
 
 
 
