7810b7a796ffc0ec6afab012a1877b8b8c5d8936
Yannick <yannickhj@gmail.com>
2018-01-26 19:26:43 +0100

    advies email form

--- advies.php
+++ advies.php
@@ -1,22 +1,6 @@
 <?php
   include_once 'header.php'
 ?>
-<?php
-if (isset($_REQUEST['email']))  {
-    //Email information
-    $admin_email = "yannickhj@gmail.com";
-    $name = $_REQUEST['name'];
-    $subject = $_REQUEST['subject'];
-    $comment = $_REQUEST['comment'];
-    //send email
-    mail($admin_email, "$subject", $comment, "From:" . $email);
-    //Email response
-    echo "Bedankt voor uw bericht.";
-    }
-    //if "email" variable is not filled out, display the form
-    else  {
-  ?>
-  
   <?php
   $sql = "SELECT * FROM leden WHERE lid_uid = '" . $_SESSION['lid_uid'] . "'";
   $result = mysqli_query($conn, $sql);
@@ -24,22 +8,23 @@ if (isset($_REQUEST['email']))  {
   ?>
    <div class="grid-x align-center">
  <div class="cell shrink form-card">
-   <form method="post">
+   <form method="post" action="includes/mail.inc.php">
    <label> Uw naam
     <input name="name" type="text" value="<?php echo '' . $row["lid_voornaam"] . ' ' . $row["lid_achternaam"] . ''?>"/>
+    </label>
+    <input name="name" type="hidden" value="<?php echo '' . $row["lid_email"] . ''?>"/>
     <label> Onderwerp
     <input name="subject" type="text" value="Advies medewerker"/>
     </label>
     <label> Uw bericht
-    <textarea name="comment" rows="15" cols="40" placeholder="Laat hier uw vraag of bericht achter voor één van onze medewerkers. "></textarea>
+    <textarea name="message" rows="10" cols="40" placeholder="Laat hier een bericht achter voor één van onze medewerkers. "></textarea>
     </label>
+    <p class="antispam">
+	<br /><input name="url" /></p>
     <input class="button" type="submit" value="Verzenden" />
     </form>
     </div>
     </div>
-  <?php
-    }
-  ?>
       <?php
   include_once 'footer.php'
 ?>
